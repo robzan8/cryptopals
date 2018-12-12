38452efa06b83f4c602272a3ad5e4cf25e29d9d2
Roberto Zanottoù <robyzan8@gmail.com>
2018-12-12 20:06:58 +0000

    challenge 2 completed.

--- set1/set1.go
+++ set1/set1.go
@@ -3,16 +3,45 @@ package cryptopals
 import (
 	"encoding/base64"
 	"encoding/hex"
-	"fmt"
-	"log"
 )
 
-// Challenge 1
-func hexToBase64(hs string) (string, error) {
-	bytes, err := hex.DecodeString(hs)
+func decodeHex(s string) []byte {
+	bytes, err := hex.DecodeString(s)
 	if err != nil {
-		return "", fmt.Errorf("hetToBase64 error: %s", err)
+		panic(err)
 	}
-	log.Println(string(bytes))
-	return base64.StdEncoding.EncodeToString(bytes), nil
+	return bytes
+}
+
+func encodeHex(bytes []byte) string {
+	return hex.EncodeToString(bytes)
+}
+
+func decodeBase64(s string) []byte {
+	bytes, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return bytes
+}
+
+func encodeBase64(bytes []byte) string {
+	return base64.StdEncoding.EncodeToString(bytes)
+}
+
+// Challenge 1
+func hexToBase64(s string) string {
+	return encodeBase64(decodeHex(s))
+}
+
+// Challenge 2
+func xor(a, b []byte) []byte {
+	if len(a) != len(b) {
+		panic("xor: slices' length mismatch")
+	}
+	res := make([]byte, len(a))
+	for i := range a {
+		res[i] = a[i] ^ b[i]
+	}
+	return res
 }
--- set1/set1_test.go
+++ set1/set1_test.go
@@ -4,12 +4,25 @@ import "testing"
 
 // Challenge 1
 func TestHexToBase64(t *testing.T) {
-	res, err := hexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
-	if err != nil {
-		t.Fatal(err)
-	}
+	in := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	res := hexToBase64(in)
 	expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 	if res != expected {
-		t.Fatalf("hexToBase64 failed challange 1. Expected %s, got %s.\n", expected, res)
+		t.Fatalf("hexToBase64 failed Challange 1. Expected %s, got %s.\n", expected, res)
 	}
+	t.Log(string(decodeHex(in)))
+}
+
+// Challenge 2
+func TestXor(t *testing.T) {
+	a := decodeHex("1c0111001f010100061a024b53535009181c")
+	t.Log(string(a))
+	b := decodeHex("686974207468652062756c6c277320657965")
+	t.Log(string(b))
+	res := encodeHex(xor(a, b))
+	expected := "746865206b696420646f6e277420706c6179"
+	if res != expected {
+		t.Fatalf("xor failed Challange 2. Expected %s, got %s.\n", expected, res)
+	}
+	t.Log(string(decodeHex(res)))
 }
