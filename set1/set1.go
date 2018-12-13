a1066e25cef378b58f505f0c998c0bcce9d272c2
Roberto Zanottoù <robyzan8@gmail.com>
2018-12-13 11:55:41 +0000

    challenge 3 completed

--- set1/set1.go
+++ set1/set1.go
@@ -3,6 +3,7 @@ package cryptopals
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"io/ioutil"
 )
 
 func decodeHex(s string) []byte {
@@ -42,6 +43,61 @@ func xor(a, b []byte) []byte {
 	res := make([]byte, len(a))
 	for i := range a {
 		res[i] = a[i] ^ b[i]
+	}
+	return res
+}
+
+// Challenge 3
+
+func frequencies(chars []byte) map[byte]float64 {
+	if len(chars) == 0 {
+		panic("Can't compute character frequencies on empty slice.")
+	}
+	freqs := make(map[byte]float64)
+	for _, c := range chars {
+		freqs[c] += 1
+	}
+	for c, f := range freqs {
+		freqs[c] = f / float64(len(chars))
+	}
+	return freqs
+}
+
+func freqsFromFile(name string) map[byte]float64 {
+	chars, err := ioutil.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+	return frequencies(chars)
+}
+
+var englishFreqs = freqsFromFile("english text.txt")
+
+func deviation(f, g map[byte]float64) float64 {
+	chars := make(map[byte]struct{})
+	for c := range f {
+		chars[c] = struct{}{}
+	}
+	for c := range g {
+		chars[c] = struct{}{}
+	}
+	var dev float64
+	for c := range chars {
+		diff := f[c] - g[c]
+		dev += diff * diff
+	}
+	return dev
+}
+
+// Not sophisticated, but distinguishes english text from random junk.
+func isEnglish(text []byte) bool {
+	return deviation(englishFreqs, frequencies(text)) <= 0.04
+}
+
+func singleXor(text []byte, c byte) []byte {
+	res := make([]byte, len(text))
+	for i := range text {
+		res[i] = text[i] ^ c
 	}
 	return res
 }
--- set1/set1_test.go
+++ set1/set1_test.go
@@ -1,6 +1,11 @@
 package cryptopals
 
-import "testing"
+import (
+	"log"
+	"math/rand"
+	"reflect"
+	"testing"
+)
 
 // Challenge 1
 func TestHexToBase64(t *testing.T) {
@@ -25,4 +30,50 @@ func TestXor(t *testing.T) {
 		t.Fatalf("xor failed Challange 2. Expected %s, got %s.\n", expected, res)
 	}
 	t.Log(string(decodeHex(res)))
+}
+
+// Challenge 3
+
+func TestFrequencies(t *testing.T) {
+	var sum float64
+	for _, f := range englishFreqs {
+		sum += f
+	}
+	if sum < 0.9 || sum > 1.1 {
+		t.Fatal("Sum of frequencies is not 1.")
+	}
+	freqs := frequencies([]byte("abcc"))
+	expected := map[byte]float64{'a': 0.25, 'b': 0.25, 'c': 0.5}
+	if !reflect.DeepEqual(freqs, expected) {
+		t.Fatalf("frequencies(\"abcc\") wrong; expected %v, got %v.\n", expected, freqs)
+	}
+}
+
+func TestDeviation(t *testing.T) {
+	f := frequencies([]byte("56i65r5rcu4ex53wwyvv8675v56echctdrh4zw"))
+	if deviation(f, f) != 0 {
+		t.Fatal("deviation(f, f) is not zero.")
+	}
+	dev := deviation(map[byte]float64{'a': 1, 'b': 1, 'c': 0.5}, map[byte]float64{'b': 1, 'c': 1, 'd': 0})
+	expected := 1 + 0 + 0.25 + 0
+	if dev != expected {
+		log.Fatalf("Wrong deviation({a:1, b:1, c:0.5}, {b:1, c:1, d:0}); expected %f, got %f.\n", expected, dev)
+	}
+	eng := "Seed uses the provided seed value to initialize the default Source to a deterministic state. If Seed is not called, the generator behaves as if seeded by Seed(1). Seed values that have the same remainder when divided by 2^31-1 generate the same pseudo-random sequence. Seed, unlike the Rand.Seed method, is safe for concurrent use."
+	t.Log(deviation(englishFreqs, frequencies([]byte(eng))))
+	r := make([]byte, 1024)
+	rand.Seed(666)
+	rand.Read(r)
+	t.Log(deviation(englishFreqs, frequencies(r)))
+}
+
+func TestChallenge3(t *testing.T) {
+	xored := decodeHex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	for i := 0; i < 256; i++ {
+		text := singleXor(xored, byte(i))
+		if isEnglish(text) {
+			t.Log(string(text))
+			t.Log(deviation(englishFreqs, frequencies(text)))
+		}
+	}
 }
